7bd60cebf6628752067b68c7c4010a4649635bab
iberis2 <snow9402@gmail.com>
2023-04-11 19:22:15 +0900

    feat 인풋 파일 삭제 버튼 추가

--- src/component/FileInput.jsx
+++ src/component/FileInput.jsx
@@ -1,11 +1,24 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef(null);
+
   const onInputChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue, nextValue.type);
   };
+
+  const handleClearClick = () => {
+    if (!inputRef.current) return;
+
+    inputRef.current.value = "";
+    onChange(name, null);
+  };
+
   return (
     <div>
-      <input type="file" onChange={onInputChange} />
+      <input ref={inputRef} type="file" onChange={onInputChange} />
+      {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
 }
